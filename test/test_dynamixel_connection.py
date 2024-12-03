c414a55ddff11386d9360bc43a62c78fe404325d
Stephen Mock <sjmock99@gmail.com>
2024-12-02 17:39:37 -0800

    Updating Cmake such that the basic connectivity test is able to run within ament_cmake_pytest

--- test/test_dynamixel_connection.py
+++ test/test_dynamixel_connection.py
@@ -16,19 +16,19 @@ class TestDynamixelConnection:
         self.packet_handler = dxl.PacketHandler(self.PROTOCOL_VERSION)
         
         # Attempt to open the port
-        self.port_open = self.port_handler.openPort()
         
-        # Set the baud rate for communication (adjust as needed)
-        self.baudrate = 3000000
-        if self.port_open:
-            self.port_handler.setBaudRate(self.baudrate)
+        # # Set the baud rate for communication (adjust as needed)
+        # self.baudrate = 3000000
+        # if self.port_open:
+        #     self.port_handler.setBaudRate(self.baudrate)
 
     def test_connection_success(self):
+        port_open = self.port_handler.openPort()
         # Test if the port is open
-        assert self.port_open, "Failed to open port"
+        assert port_open, "Failed to open port"
         
         # TODO: Not sure why the ping is not working... would have to look more into it.
-        
+
         # # Test if the motor responds to a ping (this assumes you have a motor ID, for example 1)
         # motor_id = 1  # Use the appropriate motor ID
         # comm_result, error, model_number = self.packet_handler.ping(self.port_handler, motor_id)
@@ -39,14 +39,3 @@ class TestDynamixelConnection:
         # assert comm_result == 0, "Ping failed: Communication error"
         # assert error == 0, f"Ping failed: Motor error {error}"
         # assert model_number > 0, "Ping failed: Invalid model number"
-
-    def test_connection_failure(self):
-        # Simulate a failure scenario (e.g., wrong device name or disconnected motor)
-        invalid_device_name = "/dev/serial/by-id/usb-invalid-port"
-        invalid_port_handler = dxl.PortHandler(invalid_device_name)
-        try:
-            invalid_port_open = invalid_port_handler.openPort()
-        except:
-            invalid_port_open = None
-        # Test if the invalid port fails to open
-        assert not invalid_port_open, "Port should not open with an invalid device name"
