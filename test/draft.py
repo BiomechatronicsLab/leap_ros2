c28f94dd07444b19e7761d482bfab8b8d2ba8539
Stephen Mock <sjmock99@gmail.com>
2024-12-05 18:00:02 -0800

    Cleaning up CMakeLists.txt, attempting to run a pytest implementation of an integrated ROS2 unit tests

--- test/draft.py
+++ test/draft.py
@@ -1,66 +1,92 @@
 # test_example.py
 import pytest
 import launch_testing
-import launch_pytest
-from launch_pytest.tools import process as process_tools
+import launch_testing.markers
 
-import launch
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
 import launch_ros
 import rclpy
 
-
+import unittest
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
 # from launch_ros.actions import LaunchNode
 from rclpy.node import Node
-from std_msgs.msg import String
-from sensor_msgs.msg import JointState
 
+# GLOBAL VARIABLES
+config_directory = os.path.join(get_package_share_directory('leap_ros2'), 'config')
+config_file_path = os.path.join(config_directory, "test_params.yaml")
 
-    
-@launch_pytest.fixture
-def launch_description():
+def load_yaml_file(file_path):
+    # Print the path to the YAML file
+    print(f"Loading configuration from: {file_path}")
+
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+@pytest.mark.launch_test
+@launch_testing.markers.keep_alive
+def generate_test_description():
+
+    # Set an arbitrary ROS_DOMAIN_ID so that the test is performed without inteference
+    os.environ['ROS_DOMAIN_ID'] = '42'
+    # Load parameters from the YAML file
+    config_params = load_yaml_file(config_file_path)
+
     return LaunchDescription([
         launch_ros.actions.Node(
             package='leap_ros2',  # Replace with your package name
             executable='leaphand_node.py',  # Replace with your node executable
             name='test_leap_node',
-            output='screen'
+            output='screen',
+            parameters=[config_params]
         ),
         launch_testing.actions.ReadyToTest()
     ])
 
-class TestSubscriber(Node):
+class TopicChecker(Node):
     def __init__(self):
-        super().__init__('test_subscriber_node')
-        self.subscription = self.create_subscription(
-            JointState,
-            '/leap/command_joint_states',
-            self.listener_callback,
-            10
-        )
-        self.received_message = None
+        super().__init__('topic_checker')
+    
+    def topic_exists(self, topic_name):
+        topic_list = self.get_topic_names_and_types()
+        print(topic_list)
+        for topic, _ in topic_list:
+            if topic == topic_name:
+                return True
+        return False
 
-    def listener_callback(self, msg):
-        self.received_message = msg.data
+@pytest.fixture(scope="module", autouse=True)
+def test_node():
+    """Fixture to create and manage the TopicChecker node."""
+    rclpy.init()
+    yield
+    rclpy.shutdown()
 
-@pytest.mark.launch(fixture=launch_description)
-def test_publisher_output():
-    with rclpy.init():
-        node = TestSubscriber()
-        node.start_subscriber()
-        rclpy.spin_once(node, timeout_sec=3.0)
-        assert node.received_message is not None, "Cannot hear subscription from Leap Hand" 
-        # msgs_received_flag = node.msg_event_object.wait(timeout=5.0)
-        # assert msgs_received_flag, 'Did not receive msgs !'
+@pytest.fixture(scope='function')
+def create_node():
+    """Fixture to create and manage the TopicChecker node."""
+    node = TopicChecker()
+    yield node
+    node.destroy_node()
+    rclpy.shutdown()
 
-    # with rclpy.init():
-    #     # Start the node and subscribe to the topic
-    #     test_subscriber = TestSubscriber()
-    #     # launch_service.add_action(test_subscriber)
+def test_publishes_pose(create_node):
+    """Check whether leap hand positions are being published to"""
+    print("TEST 1")
+    node = create_node
+    config_params = load_yaml_file(config_file_path)
+    topic_to_check = config_params["joint_command_topic"]
+    rclpy.spin_once(node, timeout_sec=3.0)
+    assert node.topic_exists(topic_to_check), "Cannot hear Topic:" + topic_to_check 
 
-    #     # Give time for the node to publish a message
-    #     rclpy.spin_once(test_subscriber, timeout_sec=3.0)
-
-    #     # Check if the message was received
-    #     assert test_subscriber.received_message is not None, "Cannot hear subscription from Leap Hand" 
+def test_subscribes_pose(create_node):
+    """Check whether leap hand requested positions are being subscribed to"""
+    print("TEST 2")
+    node = create_node
+    config_params = load_yaml_file(config_file_path)
+    topic_to_check = config_params["joint_feedback_topic"]
+    rclpy.spin_once(node, timeout_sec=3.0)
+    assert node.topic_exists(topic_to_check), "Cannot hear Topic:" + topic_to_check 
