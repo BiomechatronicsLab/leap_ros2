83709e65f5c538449a0f20f0d9383a5e44c9845d
Stephen Mock <sjmock99@gmail.com>
2025-01-07 17:14:05 -0800

    Adding suspendable_thread within test code, and moving old code

--- test/test_leap_node_driver.py
+++ test/test_leap_node_driver.py
@@ -9,13 +9,13 @@ import rclpy
 from rcl_interfaces.srv import SetParameters
 from rcl_interfaces.msg import Parameter, ParameterValue, SetParametersResult
 from leap_ros2.leaphand_node import LeapHandNode 
+from leap_ros2.suspendable_thread import SuspendableThread
+
+
 from rcl_interfaces.msg import ParameterType
-from rclpy.executors import MultiThreadedExecutor
-import threading
+from rclpy.executors import SingleThreadedExecutor
 import numpy as np
 
-stop_threads = False
-pause_threads = False
 node_name = "test_ros2_node"
 
 # GLOBAL VARIABLES
@@ -74,6 +74,15 @@ def parameter_setter():
     yield parameter_setter
     parameter_setter.destroy_node()
 
+def spin_for_duration(executor, duration_sec):
+    """Spin the given node for the specified duration in seconds."""
+    start_time = time.time()
+    while time.time() - start_time < duration_sec:
+        spin_node(executor)  # Adjust timeout_sec for finer granularity if needed
+
+def spin_node(executor):
+    executor.spin_once(timeout_sec=0.01)
+
 @pytest.fixture
 def leap_msg_subscriber(config_params):
     leap_msg_node = LeapMsgSubscriber(config_params)
@@ -81,28 +90,19 @@ def leap_msg_subscriber(config_params):
     leap_msg_node.destroy_node()
 
 @pytest.fixture
-def leap_ros2_node():
+def leap_ros2_node_and_thread():
     leap_ros2_node = LeapHandNode(test_flag=True, node_name=node_name)
-    yield leap_ros2_node
+    executor = SingleThreadedExecutor()
+    executor.add_node(leap_ros2_node)
+
+    suspendable_thread = SuspendableThread(target=spin_node, args=(executor, ))
+    suspendable_thread.start()
+
+    yield leap_ros2_node, suspendable_thread
+    
+    executor.shutdown()
     leap_ros2_node.destroy_node()
-
-def run_leap_node_thread(executor):
-    """Spin the given node for the specified duration in seconds."""
-    global stop_threads
-    global pause_threads
-    while True:
-        if stop_threads:
-            break
-        elif pause_threads:
-            pass
-        else:             
-            executor.spin_once(timeout_sec=0.1)
-
-def spin_for_duration(executor, duration_sec):
-    """Spin the given node for the specified duration in seconds."""
-    start_time = time.time()
-    while time.time() - start_time < duration_sec:
-        executor.spin_once(timeout_sec=0.1)  # Adjust timeout_sec for finer granularity if needed
+    suspendable_thread.kill()
 
 def set_new_params(config_params, param_names_to_check):
     params_to_set = [] # array of "sudo launch file params"
@@ -132,20 +132,13 @@ def set_new_params(config_params, param_names_to_check):
         params_to_set.append(new_param)
     return params_to_set
 
+def test_leap_node_no_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node_and_thread, parameter_setter, config_params):
 
-def test_leap_node_no_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
+    executor_test = SingleThreadedExecutor() # Will run two nodes sequentially (mutually exclusive)
+    executor_test.add_node(leap_msg_subscriber)
+    executor_test.add_node(parameter_setter)
 
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
-
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(leap_msg_subscriber)
-    executor_sub_and_client.add_node(parameter_setter)
+    leap_ros2_node, _ = leap_ros2_node_and_thread
 
     # set the params you want
     config_params["pub_current"] = False
@@ -159,21 +152,17 @@ def test_leap_node_no_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_n
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
 
     # Setup the node so that it starts publishing with updated parameters
     leap_ros2_node.setup()
     print("LEAP NODE SETUP FINISHED!")
 
     # Spin subscriber and service to receive callbacks
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 5.0)
     print("JOINT STATE EFFORT MSG!: " + str(leap_msg_subscriber.joint_state_msg.effort.tolist()))
     print("JOINT STATE POSITION MSG!: " + str(leap_msg_subscriber.joint_state_msg.position.tolist()))
     print("JOINT STATE VELOCITY MSG!: " + str(leap_msg_subscriber.joint_state_msg.velocity.tolist()))
-
-    stop_threads = True
-    time.sleep(1.0)
-
 
     assert not leap_msg_subscriber.joint_state_msg.effort, (
         f'Joint State Message is Publishing Effort when it shouldnt be'
@@ -187,20 +176,13 @@ def test_leap_node_no_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_n
         f'Joint State Message is Publishing Velocity when it shouldnt be'
     )
 
+def test_leap_node_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node_and_thread, parameter_setter, config_params):
 
-def test_leap_node_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
+    leap_ros2_node, _ = leap_ros2_node_and_thread
 
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
-
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(leap_msg_subscriber)
-    executor_sub_and_client.add_node(parameter_setter)
+    executor_test = SingleThreadedExecutor()
+    executor_test.add_node(leap_msg_subscriber)
+    executor_test.add_node(parameter_setter)
 
     # set the params you want
     config_params["pub_current"] = True
@@ -214,20 +196,17 @@ def test_leap_node_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
 
     # Setup the node so that it starts publishing with updated parameters
     leap_ros2_node.setup()
     print("LEAP NODE SETUP FINISHED!")
 
     # Spin subscriber and service to receive callbacks
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
     print("JOINT STATE EFFORT MSG!: " + str(leap_msg_subscriber.joint_state_msg.effort.tolist()))
     print("JOINT STATE POSITION MSG!: " + str(leap_msg_subscriber.joint_state_msg.position.tolist()))
     print("JOINT STATE VELOCITY MSG!: " + str(leap_msg_subscriber.joint_state_msg.velocity.tolist()))
-
-    stop_threads = True
-    time.sleep(1.0)
 
     assert leap_msg_subscriber.joint_state_msg.effort, (
         f'Joint State Message is not Publishing Effort when it should be'
@@ -241,18 +220,12 @@ def test_leap_node_publisher_joint_state_msg(leap_msg_subscriber, leap_ros2_node
         f'Joint State Message is not Publishing Velocity when it should be'
     )
 
-def test_leap_node_gain_values(leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
+def test_leap_node_gain_values(leap_ros2_node_and_thread, parameter_setter, config_params):
 
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
+    leap_ros2_node, suspendable_thread = leap_ros2_node_and_thread
 
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(parameter_setter)
+    executor_test = SingleThreadedExecutor()
+    executor_test.add_node(parameter_setter)
 
     # set the params you want
     kP = 300
@@ -270,7 +243,7 @@ def test_leap_node_gain_values(leap_ros2_node, parameter_setter, config_params):
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
 
     # Setup the node so that it starts publishing with updated parameters
     leap_ros2_node.setup()
@@ -285,7 +258,7 @@ def test_leap_node_gain_values(leap_ros2_node, parameter_setter, config_params):
     truth_kD[[0, 4, 8]] = np.ones(3) * (kD * 0.75) 
 
     # Pause the leap_node from running so that I can check the actual driver information
-    pause_threads = True
+    suspendable_thread.suspend()
     time.sleep(1.0)
 
     # Check the driver
@@ -293,28 +266,17 @@ def test_leap_node_gain_values(leap_ros2_node, parameter_setter, config_params):
     test_kI = leap_ros2_node.dynamixel_mgr.get_kI(leap_ros2_node.dynamixel_mgr.motor_ids)
     test_kD = leap_ros2_node.dynamixel_mgr.get_kD(leap_ros2_node.dynamixel_mgr.motor_ids)
 
-    # Stop the thread prior to assertion
-    stop_threads = True
-    time.sleep(1.0)
-
     # have to convert to ints because thats how it is actually sent to the dynamixels
     assert truth_kP.astype(int).tolist() == test_kP
     assert truth_kI.astype(int).tolist() == test_kI
     assert truth_kD.astype(int).tolist() == test_kD
 
 
-def test_leap_node_start_position(leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
+def test_leap_node_start_position(leap_ros2_node_and_thread, parameter_setter, config_params):
+    leap_ros2_node, suspendable_thread = leap_ros2_node_and_thread
 
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
-
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(parameter_setter)
+    executor_test = SingleThreadedExecutor()
+    executor_test.add_node(parameter_setter)
 
     # set the initial conditions that you want (parameters much be a DOUBLE array)
     truth_start_pos_deg = [20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.00, 20.0, 20.0] # grasp
@@ -327,7 +289,7 @@ def test_leap_node_start_position(leap_ros2_node, parameter_setter, config_param
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
     print(set_parameter_results)
 
     # Setup the node so that it starts publishing with updated parameters
@@ -335,37 +297,26 @@ def test_leap_node_start_position(leap_ros2_node, parameter_setter, config_param
     print("LEAP NODE SETUP FINISHED!")
     
     # Pause the leap_node from running so that I can check the actual driver information
-    pause_threads = True
+    suspendable_thread.suspend()
     time.sleep(1.0)
     test_start_pos_deg = leap_ros2_node.dynamixel_mgr.get_position_deg(leap_ros2_node.dynamixel_mgr.motor_ids)
     leap_ros2_node.dynamixel_mgr.set_goal_position_deg(leap_ros2_node.dynamixel_mgr.motor_ids, np.zeros(len(leap_ros2_node.dynamixel_mgr.motor_ids))) # just reset it back to home, not 100% necessary...
-
-    # Stop the thread prior to assertion
-    stop_threads = True
-    time.sleep(1.0)
 
     # Now actually check without the node
     position_comparison = [abs(a - b) for a, b in zip(truth_start_pos_deg, test_start_pos_deg)]
     print(position_comparison)
 
-    # tolerance of 10 degrees, etc (could be adjusted)
+    # tolerance of 15 degrees, etc (could be adjusted)
     comparison_result = [comparison < 15.0 for comparison in position_comparison]
 
     # have to convert to ints because thats how it is actually sent to the dynamixels
     assert all(comparison_result)
 
-def test_leap_node_no_device_name(leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
+def test_leap_node_no_device_name(leap_ros2_node_and_thread, parameter_setter, config_params):
+    leap_ros2_node, _ = leap_ros2_node_and_thread
 
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
-
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(parameter_setter)
+    executor_test = SingleThreadedExecutor()
+    executor_test.add_node(parameter_setter)
 
     # remove the device_name parameter to cause the exception to be raised!
     del config_params["device_name"] 
@@ -377,35 +328,19 @@ def test_leap_node_no_device_name(leap_ros2_node, parameter_setter, config_param
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
 
     # Assert that a ValueError is raised
     with pytest.raises(ValueError, match="Please state the device_name in the configuration file."):
         # Setup the node so that it starts publishing with updated parameters
         leap_ros2_node.setup()
 
-    # Pause the leap_node from running so that I can check the actual driver information
-    pause_threads = True
-    time.sleep(1.0)
-
-    # Stop the thread prior to assertion
-    stop_threads = True
-    time.sleep(1.0)
-
     assert True
 
-def test_leap_node_no_dynamixel_type(leap_ros2_node, parameter_setter, config_params):
-    global stop_threads, pause_threads
-    stop_threads = False
-    pause_threads = False
-
-    executor_leap_ros2_node = MultiThreadedExecutor()
-    executor_leap_ros2_node.add_node(leap_ros2_node)
-    thread1 = threading.Thread(target=run_leap_node_thread, args=(executor_leap_ros2_node,))
-    thread1.start()
-
-    executor_sub_and_client = MultiThreadedExecutor()
-    executor_sub_and_client.add_node(parameter_setter)
+def test_leap_node_no_dynamixel_type(leap_ros2_node_and_thread, parameter_setter, config_params):
+    leap_ros2_node, _ = leap_ros2_node_and_thread
+    executor_test = SingleThreadedExecutor()
+    executor_test.add_node(parameter_setter)
 
     # remove the device_name parameter to cause the exception to be raised!
     del config_params["dynamixel_type"] 
@@ -417,19 +352,11 @@ def test_leap_node_no_dynamixel_type(leap_ros2_node, parameter_setter, config_pa
     set_parameter_results = parameter_setter.send_request(SetParameters.Request(parameters=params_to_set))
 
     # Spin subscriber and service to actually execute the service call
-    spin_for_duration(executor_sub_and_client, 1.0)
+    spin_for_duration(executor_test, 1.0)
 
     # Assert that a ValueError is raised
     with pytest.raises(ValueError, match="Please state the dynamixel_type in the configuration file."):
         # Setup the node so that it starts publishing with updated parameters
         leap_ros2_node.setup()
-
-    # Pause the leap_node from running so that I can check the actual driver information
-    pause_threads = True
-    time.sleep(1.0)
-
-    # Stop the thread prior to assertion
-    stop_threads = True
-    time.sleep(1.0)
-
+        
     assert True
